0706ed17b5bac1cda78e90e7ef772ef5cfd0b3f6
Riley <ryeakle@gmail.com>
2015-04-25 18:22:47 -0700

    basic segmentation working? still slow

--- ExposureCorrector.py
+++ ExposureCorrector.py
@@ -15,21 +15,45 @@ import scipy.sparse as spsparse
 		# NEWEST ISSUE:
 		# Need to deal with max internal weights in adjacency
 
-		# TODO: solve out ground truth for test image
+		# ----------------------------------------------------------
+		# TODO: when merging, grow a single component as much as possible before 
+		# moving on 
+		# use a queue of pixels to check!
+		# ---------------------------------------------------------
 
 class ExposureCorrector():
 	img = []
 	adj_mat = []
 	# ^ this guy represents adjacency of COMPONENTS, not individual pixels
-	# the weights indicate the minimum weight between neighboring components
-	# diagonal = max internal weight
+	# 0 == not connected
+	# 1 == connected
 
+	# Hash of components (ie the segmentation)
 	components = {}
-	# self.internal_weights = {}
+	
+	# pairwise weights of connected components
+	weights = {}
+	
 	w = 0
 	h = 0
 	
-	k = 300
+	k = 25
+
+	def show_segmentation(self):
+		out_img = np.zeros((self.h, self.w)).astype(np.uint8)
+		for c in self.components:
+			pixels = self.components[c]
+			pix_list = []
+			for p in pixels:
+				pix_list.append(self.img[p[0], p[1]])
+
+			mean_val = int(np.mean(pix_list))
+
+			for p in pixels:
+				out_img[p[0], p[1]] = mean_val
+
+		return out_img
+
 
 	def get_adjacency_matrix(self, img):
 		self.img = img
@@ -44,17 +68,19 @@ class ExposureCorrector():
 
 		diag_h = np.tile(h_connections, (rows, 1))
 		diag_h = diag_h[0:-1]
-		print diag_h.shape
+		# print diag_h.shape
 		assert(diag_h.shape[0] == rows*cols - 1)
 		
 		diag_v = np.ones((cols*(rows-1), 1))
-		print diag_v.shape
+		# print diag_v.shape
 		assert(diag_v.shape[0] == rows*cols - cols)
 
 		diagonals = [diag_h.T, diag_v.T]
 
 		self.adj_mat = spsparse.diags(diagonals, [1, cols], shape=(rows*cols, rows*cols), format='lil', dtype=np.uint8)
 		
+		self.adj_mat = (self.adj_mat + self.adj_mat.transpose()).tolil()
+		# print spsparse.isspmatrix_lil(self.adj_mat)
 		return self.adj_mat
 
 	def init_components(self):
@@ -70,13 +96,13 @@ class ExposureCorrector():
 		return self.components
 
 
-	def init_adj_weights(self):
+	def init_weights(self):
 		# at this point each pixel is in it's own component
 
 		# for each pixel, initial weight = abs(pix[i] - pix[j])
 		nonzero = self.adj_mat.nonzero()
-		print nonzero[0]
-		print nonzero[1]		
+		# print nonzero[0]
+		# print nonzero[1]		
 
 		for i in range(len(nonzero[0])):
 			home_pix = nonzero[0][i]
@@ -88,34 +114,29 @@ class ExposureCorrector():
 			neighbor_i = self.pix_num_to_index(neighbor)
 			neighbor_val = self.img[neighbor_i[0], neighbor_i[1]]
 
-			self.adj_mat[home_pix, neighbor] = abs(home_val - neighbor_val)
+			self.weights[(home_pix, neighbor)] = abs(int(home_val) - int(neighbor_val))
+			self.weights[(neighbor, home_pix)] = self.weights[(home_pix, neighbor)]
 
-		# for i in range(len(self.components.keys())):
-		# 	self.adj_mat[i, i] = 0
+		for i in range(len(self.components.keys())):
+			self.weights[(i, i)] = 0
 
 	def segment_img(self):
 		num_changes = 1
-		while (num_changes != 0):
-			keys_to_delete = []
-			num_changes = 0
+	
+		components = self.components.keys()
 
-			components = self.components.keys()
-
-			for c in components:
+		for c in components:
+			while(num_changes > 0):
 				if (c in self.components):
-					# keys = self.update_component(c)
-					num_changes += self.update_component(c)
+					num_changes = self.update_component(c)
+				else:
+					num_changes = 0
 					
-					# for j in keys:
-					# 	keys_to_delete.append(j)
-			
-			# Delete Keys from merges:
-			# for k in keys_to_delete:
-			# 	self.components.pop(k, None)
+			print "Component: " + str(c)
 
-			print "Number of merges: " + str(num_changes)
-			print "Total components: " + str(len(self.components.keys()))
-			print ""
+		# print "Number of merges: " + str(num_changes)
+		# print "Total components: " + str(len(self.components.keys()))
+		# print ""
 
 		return self.components
 
@@ -124,15 +145,25 @@ class ExposureCorrector():
 		keys_to_delete = []
 		num_changes = 0
 		adj_components = self.adj_mat.getrow(c1).nonzero()
-		print adj_components[1]
-		for c2 in adj_components[1]:
+		# print adj_components[1]
+		components_to_check = list(adj_components[1])
+
+		while components_to_check != []:
 			# skip if we deleted component already
+			c2 = components_to_check.pop(0)
 			if (c2 != c1) and (c2 in self.components):
 				if (self.check_boundary(c1, c2, self.k) == False):
+					# print "merged " + str(c1) + " and " + str(c2)
+					
+					new_components = (self.adj_mat.getrow(c2).nonzero())[1]
+
 					self.merge_components(c1, c2)
-					print "merged " + str(c1) + " and " + str(c2)
+					
+					# print new_components
+					components_to_check = components_to_check + list(new_components)
+					# print "merged " + str(c1) + " and " + str(c2)
 					num_changes += 1
-					# keys_to_delete.append(c2)
+					
 
 
 		return num_changes
@@ -140,9 +171,9 @@ class ExposureCorrector():
 
 	def check_boundary(self, c1, c2, k):
 		boundary = True
-		m_int = min([self.adj_mat[c1, c1] + k/len(self.components[c1]), self.adj_mat[c2, c2] + k/len(self.components[c2])]) 
+		m_int = min([self.weights[(c1, c1)] + k/len(self.components[c1]), self.weights[(c2, c2)] + k/len(self.components[c2])]) 
 		
-		if self.adj_mat[c1, c2] > m_int:
+		if self.weights[(c1, c2)] > m_int:
 			boundary = True
 		else:
 			boundary = False
@@ -150,49 +181,65 @@ class ExposureCorrector():
 		return boundary
 
 	def merge_components(self, c1, c2):
+
 		# copy all elements of c2 into c1 
-		to_copy = self.components[c2]
-		for pixel in to_copy:
-			self.components[c1].append(pixel)
+		# print "copying components"
+		self.components[c1] = self.components[c1] + self.components[c2]
+		# print "done"
 
 		# copy adjacency matrix info
+		# print "merging adj mat"
 		self.merge_adj_mat(c1, c2)
+		# print "done"
 
 		# delete component in component hash
 		self.components.pop(c2, None)
 
-		print self.adj_mat.toarray()
+		# print self.adj_mat.toarray()
 		return 0
 
 	def merge_adj_mat(self, c1, c2):
-		c1_adj = self.adj_mat.getrow(c1)
+		# c1_adj = self.adj_mat.getrow(c1)
 		c2_adj = self.adj_mat.getrow(c2)
 
-		c2_nonzero = c2_adj.nonzero()
+		c2_nonzero = c2_adj.nonzero()[1]
 
-		for component in c2_nonzero[1]:
-			if component == c1:
-				print "update max"
-				# update max internal weight
-				c1_adj[0, component] = max(c1_adj[0, component], c2_adj[0, component])
+
+		# print "updating max internal weight"
+		# update max internal weight:
+		self.weights[(c1, c1)] = max(self.weights[(c1, c1)], self.weights[(c1, c2)])
+
+
+		# print "updating weights and neighbors"
+		for component in c2_nonzero:
+			# update connection to neighbors
+			if (self.adj_mat[c1, component] == 1) or (c2_adj[0, component] == 1):
+				self.adj_mat[c1, component] = 1
+			
+			# update min neighbor weights
+			if ((c1, component) in self.weights) and ((c2, component) in self.weights):
+				self.weights[(c1, component)] = min([self.weights[(c1, component)], self.weights[(c2, component)]])
+				self.weights[(component, c1)] = self.weights[(c1, component)]
+				
+				# delete old component weights
+				self.weights.pop((c2, component), None)
+				self.weights.pop((component, c2), None)
+			
+			elif ((c2, component) in self.weights):
+				self.weights[(c1, component)] = self.weights[(c2, component)]
+				self.weights[(component, c1)] = self.weights[(c1, component)]
+
+				# delete old component weights
+				self.weights.pop((c2, component), None)
+				self.weights.pop((component, c2), None)
 			else:
-				# update minimum connection to neighbors
-				if (c1_adj[0, component] == 0):
-					c1_adj[0, component] = c2_adj[0, component]
-				elif (c2_adj[0, component] == 0): 
-					c1_adj[0, component] = c1_adj[0, component]
-				else:
-					c1_adj[0, component] = min(c1_adj[0, component], c2_adj[0, component])
-					
+				# retain current min weight
+				pass
 
-
-		self.adj_mat[c1, :] = c1_adj
-
-		# delete c2's row and col in adj_mat?
-		# for now, zero out
-		self.adj_mat[c2, :] = 0
-		self.adj_mat[:, c2] = 0
-
+			# clear old neighbor info
+			self.adj_mat[c2, component] = 0
+			self.adj_mat[component, c2] = 0	
+			
 		return 0
 
 	
--- test_ExposureCorrector.py
+++ test_ExposureCorrector.py
@@ -3,10 +3,12 @@
 
 import numpy as np
 import ExposureCorrector as EC
+import cv2
 
 
+# img = np.array([[0, 10, 255], [5, 200, 255]])
 
-img = np.array([[0, 10, 255], [5, 200, 255]])
+img = cv2.imread("test_img_600x450.png", cv2.CV_LOAD_IMAGE_GRAYSCALE)
 
 corrector = EC.ExposureCorrector()
 
@@ -14,16 +16,34 @@ corrector = EC.ExposureCorrector()
 
 def main():
 	adj = corrector.get_adjacency_matrix(img)
-	print adj.toarray()
-	corrector.init_components()
-	print corrector.components
 	
-	corrector.init_adj_weights()
 
-	print corrector.adj_mat.toarray()
+	# print adj.toarray()
+	
 
-	corrector.segment_img()
+	corrector.init_components()
+	# print corrector.components
+	
+	corrector.init_weights()
+	# print corrector.weights
 
+	# print corrector.adj_mat.toarray()
+
+	components = corrector.segment_img()
+
+	# print components
+
+	cv2.namedWindow("OG")
+	cv2.namedWindow("Segmentation")
+
+	seg_img = corrector.show_segmentation()
+
+	cv2.imshow("OG", img)
+	cv2.imshow("Segmentation", seg_img)
+
+	cv2.imwrite("seg_img.jpg", seg_img)
+
+	cv2.waitKey(0)
 	return 0
 
 if __name__ == "__main__":
